01f59184d73e166102abed8b4bdc576a03186e94
lowerants <156000131+lowerants@users.noreply.github.com>
2025-03-27 15:25:13 -0400

    CourseControls simplified
    CourseControls now only can take a single course as a prereq instead of an ArrayList

--- src/CourseControls.java
+++ src/CourseControls.java
@@ -5,69 +5,44 @@ import java.util.ArrayList;
 public class CourseControls {
     // courses.txt will have these as an ArrayList of requirements for them
 
-    private ArrayList<String> courses; // when the control is taking another course
+    private String course; // when the control is taking another course
     // for now, I won't worry about concurrent vs prereq (probably won't do it for the final assignment either)
     // String instead of Course makes this WAY easier
 
     private int sem; // when a class can only be taken a certain semester or later
 
-    private ArrayList<College> college; // when a course can only be taken by students in a certain college
 
     // when constructing, 2 of these will likely be null, as many courses.txt only have one of these as a CourseControl
     // better design is to probably make 3 more constructors, each with one parameter, but Idc rn
-    public CourseControls(ArrayList<String> courses, int sem, ArrayList<College> college) {
-        this.courses = courses;
+    public CourseControls(String course, int sem) {
+        this.course = course;
         this.sem = sem;
-        this.college = college;
     }
 
     public CourseControls(String course) {
-        this.courses = new ArrayList<>();
-        this.courses.add(course);
+        this.course = course;
         this.sem = 1;
     }
 
     @Override
     public String toString() {
         ArrayList<String> str = new ArrayList<>();
-        if(this.courses != null) {
-            str.add(this.courses.toString());
+        if(!this.course.isEmpty()) {
+            str.add(this.course);
         }
         if(this.sem != 1) {
-            str.add(this.sem + "");
-        }
-        if(this.college != null) {
-            str.add(college.toString());
+            str.add(" " + this.sem);
         }
 
         return str.toString();
-        // maybe add line breaks?
-
-        /*
-        *
-        * case Ast.Expr.Add(Ast.Expr left, Ast.Expr right) -> {
-                isolateVars(vars, left);
-                isolateVars(vars, right);
-                yield vars;
-            }*/
     }
 
-    public ArrayList<String> getCourses() {
-        return courses;
+    public String getCourses() {
+        return course;
     }
 
     public int getSem() {
         return sem;
     }
 
-    // for displaying the Colleges that a Student must be in to be in the class
-    public ArrayList<College> getCollege() {
-        return college;
-    }
-
-    // this would only be for updating the requirements set by the university and not function the Student should have
-    // likely REMOVING this
-    public void setCollege(ArrayList<College> college) {
-        this.college = college;
-    }
 }
--- src/TestHarness.java
+++ src/TestHarness.java
@@ -13,28 +13,64 @@ import java.io.File;
 
 public class TestHarness {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+        CourseControls cc = new CourseControls("ist130", 1);
 
+        Course ist = new Course("App Dev Studio 1", "ist261", 3, false
+        cc, 1
+        {});
+
+
+        /*
+
+    public String toString() {
+        return name + " " +
+                "Courses: " + courses + "\n" +
+                totalCredits + " credits";
+
+    }
+
+    public ArrayList<Course> getCourses() {
+        return this.courses;
+    }
+
+
+    // todo: add validation like in the graduation plan
+    // temporary, mostly a reminder to updated totalCredits HERE
+    public void addCourse(Course c) {
+        courses.add(c);
+        totalCredits += c.getNumCredits();
+    }
+
+    // todo: add reminders for the consequences of removing? That might be saved in a list when changing courses
+    public void removeCourse(Course c) {
+        totalCredits -= c.getNumCredits();
+        courses.remove(c);
+    }
+
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+         */
+    }
+
+    public TestHarness() throws FileNotFoundException{
         File courses = new File("C:\\Users\\misce\\OneDrive\\Documents\\GitHub\\ScheduleBurger\\text_files\\courses.txt");
         CourseList cL = new CourseList("All Courses", courses);
 
         System.out.println("test:");
         System.out.println(cL.toString());
+
+
     }
+
 
     @Test
     public void createCourse() throws FileNotFoundException {
 //        SemesterPlan semPlan = new SemesterPlan(Semester.FALL, 2025, new ArrayList<>());
 //        semPlan.addCourse()
 
-        File courses = new File("C:\\Users\\misce\\OneDrive\\Documents\\GitHub\\ScheduleBurger\\text_files\\courses.txt");
-        CourseList cL = new CourseList("All Courses", courses);
 
-        Assertions.assertEquals("All Courses Courses: [IST261: App Dev Studio 1 [3]]\n" +
-                "3 credits", cL.toString());
-        Assertions.assertEquals("IST261: App Dev Studio 1 [3]" +
-            "\nCourse Controls: [[{ist140}]]"
-            + "\nAttributes: []",
-                cL.getCourses().get(0).fullString());
     }
 }
